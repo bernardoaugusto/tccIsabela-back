a6f669229c801391cb54beafde42659ddc2b805f
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2020-10-20 17:09:14 -0300

    script para ver se tem registro naquela hora

--- src/bora/loop.ts
+++ src/bora/loop.ts
@@ -1,17 +1,36 @@
+/* eslint-disable no-var */
 import { getRepository } from 'typeorm';
 import axios from 'axios';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-export const getByDay = async (): Promise<Appointment[]> => {
+let aux: number | null = null;
+
+export const getByDay = async (): Promise<Appointment[] | undefined> => {
   const today = new Date();
   const day = today.getDate();
-  const month = today.getMonth();
+  const month = today.getMonth() + 1;
   const year = today.getFullYear();
+  const hour = today.getHours() + 3;
 
-  return getRepository(Appointment).query(
-    `select * from appointments where date BETWEEN '2020-10-19' AND '2020-10-20'`,
-  );
+  // console.log(day);
+  console.log(`${day}-${month}-${year} : ${hour}`);
+
+  if (aux !== hour) {
+    aux = hour;
+
+    return getRepository(Appointment).query(
+      `select *
+      from appointments a
+      where date_part('year', a.date) = '${year}'
+      and date_part('month', a.date) = '${month}'
+      and date_part('day', a.date) = '${day}'
+      and date_part('hour', a.date) = '${hour}'
+      `,
+    );
+  }
+
+  return undefined;
 };
 
 const loop = async (): Promise<void> => {
